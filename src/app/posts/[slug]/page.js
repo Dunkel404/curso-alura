690b87e2a43ceff71218d7c25458b7dacf652634
Dunkel <89481059+Dunkel404@users.noreply.github.com>
2026-01-19 13:35:03 +0000

    markdown render em estado inicial

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -1,4 +1,7 @@
 import logger from "@/logger";
+import { remark } from "remark";
+import html from 'remark-html';
+import DOMPurify from 'dompurify'
 
 async function GetPost({ slug }) {
   try {
@@ -6,8 +9,19 @@ async function GetPost({ slug }) {
     const data = await response.json();
     if (!response || !response.ok) throw Error("slug not found or connection error");
     if (data.lenght === 0) throw Error("nothing found");
-    
     logger.info(`data reached successfuly!`);
+    
+    const processedContent = await remark()
+    .use(html)
+    .process(data.markdown);
+    
+    const contentHtml = processedContent.toString();
+    
+
+    //     const sanitizedHtml = DOMPurify.sanitize(processedContent.toString())
+    // const contentHtml = sanitizedHtml;
+
+    // data[0].markdown = contentHtml;
     return data[0];
   } catch {
     logger.error("Ops! slug not found or connection error");
@@ -17,7 +31,11 @@ async function GetPost({ slug }) {
 
 const PagePost = async ({ params }) => {
   const post = await GetPost(params);
-  return <h1> {post.title} </h1>;
+  return (
+    <>
+    <div style={{padding: '16px', 'background-color': 'white'}} dangerouslySetInnerHTML={{__html: post.markdown}} />
+    </>
+  )
 };
 
 export default PagePost;
